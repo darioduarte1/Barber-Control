39e8124f39507eadebb28453914d361adf470473
darioduarte1 <darioduarte1991@gmail.com>
2024-12-15 13:17:47 +0100

    finalizacao de implementacao de base de dados y correcion de errores - DB is Up

--- ws/database.js
+++ ws/database.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb+srv://salao:ZUkhbau4HqnhfiTY@clusterdev.1tphl.mongodb.net/?retryWrites=true&w=majority&appName=ClusterDev'; 
+const URI = 'mongodb+srv://salao:ZUkhbau4HqnhfiTY@clusterdev.1tphl.mongodb.net/?retryWrites=true&w=majority&appName=ClusterDev';
 
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-
+// Conexión a MongoDB
 mongoose
-    .connect(URI)
-    .then (() => console.log('DB is Up!'))
-    .catch(() => console.log(err));
+  .connect(URI)
+  .then(() => console.log('DB is Up!'))
+  .catch((err) => console.error('DB connection error:', err));
